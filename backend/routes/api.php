915f25bbf45caf130a7df2503eaf0b5634524a0f
Wanderson Soares <wandersonsoares841@gmail.com>
2025-09-27 15:34:26 -0300

    feat: add routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MeliNotificationController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::get('/test', function () {
     return response()->json(['message' => 'LogManager API']);
 });
+
+Route::post('/notifications/meli', [MeliNotificationController::class, 'handle'])->name('meli.notifications');
