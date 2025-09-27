5423cf302017e84c55bd2d5f921f87622b2636b1
Wanderson Soares <wandersonsoares841@gmail.com>
2025-09-27 15:38:04 -0300

    feat: add rota de etiquetas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MeliNotificationController;
+use App\Http\Controllers\Api\ShippingLabelController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,7 @@ Route::get('/test', function () {
 });
 
 Route::post('/notifications/meli', [MeliNotificationController::class, 'handle'])->name('meli.notifications');
+
+Route::get('/orders/{order}/label', [ShippingLabelController::class, 'show'])
+    ->name('orders.label.show')
+    ->middleware('auth:sanctum');
