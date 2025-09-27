6d98d508d5ca4bd8fa91e4b28f7f1580a00b0a88
Wanderson Soares <wandersonsoares841@gmail.com>
2025-09-27 16:38:34 -0300

    feat: add rotas de sincronizacao

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\MeliAuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/auth/meli/redirect', [MeliAuthController::class, 'redirect'])->name('meli.auth.redirect');
+Route::get('/auth/meli/callback', [MeliAuthController::class, 'callback'])->name('meli.auth.callback');
