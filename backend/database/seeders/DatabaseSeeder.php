a214a58facf81d3fc98698bb46a515c2c68af7ad
Wanderson Soares <wandersonsoares841@gmail.com>
2025-09-29 09:40:44 -0300

    feat: add dados de teste

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,33 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
+use App\Models\OrderItem;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        if (User::count() === 0) {
+            $this->command->error('Nenhum usuário encontrado. Por favor, se autentique pelo menos uma vez antes de executar o seeder.');
+            return;
+        }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->command->info('A criar 25 pedidos de teste com itens...');
+
+        // Cria 25 pedidos
+        Order::factory(25)
+            ->create()
+            ->each(function ($order) {
+                // Para cada pedido criado, cria entre 1 a 5 itens de pedido aleatoriamente
+                OrderItem::factory(rand(1, 5))->create([
+                    'order_id' => $order->id,
+                ]);
+            });
+
+        $this->command->info('Pedidos de teste criados com sucesso!');
     }
 }
